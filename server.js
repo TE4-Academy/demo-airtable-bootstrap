2457b887caf4f0fee6a27e3c6457fadf9d012755
herkommer <herkommer@users.noreply.github.com>
2023-10-25 22:24:50 +0200

    Removed earlier demo code, making it cleaner

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const Airtable = require('airtable');
-const base = new Airtable({apiKey: 'pat47w1lvZu5bF8Ac.c3e87e9af452ede6cccadf640ce9eb9a8b4dd0178dad669e123980238889ddf6'}).base('appftiYaMNci71Mcf');
 
+const base = new Airtable({apiKey: 'pat47w1lvZu5bF8Ac.c3e87e9af452ede6cccadf640ce9eb9a8b4dd0178dad669e123980238889ddf6'}).base('appftiYaMNci71Mcf');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,12 +11,6 @@ app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.sendFile('index');
-});
-
-app.get('/data', (req,res) => {
-    fs.readFile('data.json', 'utf-8', (err, data) => {
-        res.json(JSON.parse(data));
-    } );
 });
 
 app.get('/contacts',(req,res)=>{
